48b102b2aea51216c09c2b508f67bfa895c8075c
Quang Vo <vovanquangnbk@gmail.com>
2024-01-07 16:46:02 +0700

    add fastapi

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from app.app import handler
 from typing import Union
 from fastapi import FastAPI
+from fastapi import HTTPException
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return "Hello!"
+def hello():
+    return {"Hello": "World"}
 
 @app.post("/query")
 def query(research_filed: str, topk: int):
     result = handler(research_filed, topk)
+    if not research_filed or not topk:
+        raise HTTPException(status_code=404, detail="Research field and topk are required")
     return {"result": result}
 
